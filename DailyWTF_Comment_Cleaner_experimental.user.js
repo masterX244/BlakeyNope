ceca6ea5778fc3335a9618e5241a3cbcf532705c
Mike Unfried <styrofoamboy@users.noreply.github.com>
2016-12-01 14:26:31 -0600

    Update DailyWTF_Comment_Cleaner_experimental.user.js
    Ask and ye shall receive:  here is a version of the script which contains a basic content-level filtering engine :)
    
    Removed some names from the list that are no longer necessary, due to the new case-insensitive "Contains" function.
    I also removed my name :)

--- DailyWTF_Comment_Cleaner_experimental.user.js
+++ DailyWTF_Comment_Cleaner_experimental.user.js
@@ -8,26 +8,39 @@
 // @grant        none
 // ==/UserScript==
 
+// Creates a case-insensitive version of the ':contains' selector
+jQuery.expr[':'].Contains = jQuery.expr.createPseudo(function(arg) {
+    return function ( elem ) {
+        return jQuery(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+    };
+});
 (function() {
     'use strict';
-    var names = ['B L A K E Y R A T','Blakeyrat','b?keyrat','blameyrat','Fuck you alex','bläkeyrat','blockyrat','Ulysses','B lALye key RraBlaRAT','B lALye key RraBlaRAT',
-                 'Bla-key-rat','blaKEY RaT','CHARLIEMOUSE','TenshiNo','b l a k e y r a t','blakeyrat','BonkeyRatt','BarfyRoot',
+    var names = ['B L A K E Y R A T','b?keyrat','blameyrat','Fuck you alex','bläkeyrat','blockyrat','Ulysses','B lALye key RraBlaRAT',
+                 'Bla-key-rat','blaKEY RaT','CHARLIEMOUSE','blakeyrat','BonkeyRatt','BarfyRoot',
                 'bakedrat'];
-    var contains = '';
-    for (var i = 0; i < names.length; i++) {
-        if (i > 0) contains += ", ";
-        contains += "span.poster:contains('" + names[i] + "')";
+    var nameList = '';
+    var i;
+    for (i = 0; i < names.length; i++) {
+        if (i > 0) nameList += ", ";
+        nameList += "span.poster:Contains('" + names[i] + "')";
     }
-    //.comments>.comment { max-height:20em; overflow-y:auto; }
+    var illegalText = ['this website sucks', 'delete this fucking website'];
+    var textList = '';
+    for(i = 0; i < illegalText.lenth; i++) {
+        if (i > 0) textList += ", ";
+        textList += "div:Contains('" + illegalText[i] + "')";
+    }
     $(".comments > .comment").css({maxHeight: '20em', overflow: 'auto' });
-    //.comments>.comment>div>img{display:none}
-    $(".comments > .comment > div > img").css("display", "none");
+    $(".comments > .comment img").css("display", "none");
     $("li.comment").filter(function(index) {
+        var hasIllegalText = false;
+        for(var i = 0; i < illegalText.length; i++) {
+            if($(this).find(":Contains('" + illegalText[i] + "')").length > 0)
+                hasIllegalText = true;
+        }
         return (
-            $(this).children(contains).length > 0 &&
+            ($(this).children(nameList).length > 0 || hasIllegalText) &&
             $(this).children("span.poster-anon:contains('(unregistered)')").length > 0);
     }).hide();
-    $(function(){
-        $('*').remove('img[src*="meatspin"]');
-    });
 })();
