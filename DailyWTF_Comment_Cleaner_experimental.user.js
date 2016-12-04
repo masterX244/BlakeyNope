653ea6ebf42e9c7c1be243a46e8a069456084d14
LH <masterX244@gmail.com>
2016-12-04 11:19:19 +0100

    added some more aliases & another filterwordy

--- DailyWTF_Comment_Cleaner_experimental.user.js
+++ DailyWTF_Comment_Cleaner_experimental.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DailyWTF Comment Cleaner
 // @namespace    https://github.com/masterX244
-// @version      0.2
+// @version      0.3
 // @description  Sterilize TDWTF Comments. Experimental Edition with untested changes. May not work
 // @author       Mike Unfried
 // @match        http://thedailywtf.com/articles/comments/*
@@ -16,10 +16,10 @@ jQuery.expr[':'].Contains = jQuery.expr.createPseudo(function(arg) {
 });
 (function() {
     'use strict';
-    var illegalText = ['this website sucks', 'delete this fucking website'];
+    var illegalText = ['this website sucks', 'delete this fucking website','FUCKING WEBSITE'];
     var names = ['B L A K E Y R A T','b?keyrat','blameyrat','Fuck you alex','bläkeyrat','blockyrat','Ulysses','B lALye key RraBlaRAT',
                  'Bla-key-rat','blaKEY RaT','CHARLIEMOUSE','blakeyrat','BonkeyRatt','BarfyRoot',
-                'bakedrat'];
+                'bakedrat','b!L ??? Ake ySD RATTT'];
     var nameList = '';
     var i;
     for (i = 0; i < names.length; i++) {
